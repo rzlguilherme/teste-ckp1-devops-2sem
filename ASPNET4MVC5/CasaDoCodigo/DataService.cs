52e1103686f35ea44fca419cb7a519493b7aea43
rzlguilherme <58034832+rzlguilherme@users.noreply.github.com>
2023-09-08 19:54:29 -0300

    Add files via upload

---
+++ ASPNET4MVC5/CasaDoCodigo/DataService.cs
@@ -0,0 +1,45 @@
+﻿using CasaDoCodigo.Models;
+using CasaDoCodigo.Repositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Hosting;
+
+namespace CasaDoCodigo
+{
+    interface IDataService
+    {
+        void InicializaDB();
+    }
+
+    class DataService : IDataService
+    {
+        private readonly ApplicationContext contexto;
+        private readonly IProdutoRepository produtoRepository;
+
+        public DataService(ApplicationContext contexto,
+            IProdutoRepository produtoRepository)
+        {
+            this.contexto = contexto;
+            this.produtoRepository = produtoRepository;
+        }
+
+        public void InicializaDB()
+        {
+            contexto.Database.CreateIfNotExists();
+
+            List<Livro> livros = GetLivros();
+
+            produtoRepository.SaveProdutos(livros);
+        }
+
+        private static List<Livro> GetLivros()
+        {
+            var json = File.ReadAllText(HostingEnvironment.MapPath(@"~/App_Data/livros.json"));
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+    }
+
+
+}
