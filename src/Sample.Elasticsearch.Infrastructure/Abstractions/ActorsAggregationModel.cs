07b0adc309850b00578e9bcf5c2447551afd2fcc
rzlguilherme <58034832+rzlguilherme@users.noreply.github.com>
2023-09-08 19:29:05 -0300

    Delete src directory

--- src/Sample.Elasticsearch.Infrastructure/Abstractions/ActorsAggregationModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sample.Elasticsearch.Infrastructure.Abstractions
-{
-    public class ActorsAggregationModel
-    {
-        public double TotalAge { get; set; }
-        public double TotalMovies { get; set; }
-        public double AverageAge { get; set; }
-    }
-}
